34ca8cc5dc68d48fbc64fb9aed526002265a2df2
Diya Saha <137777156+diya-02042003@users.noreply.github.com>
2024-09-01 03:19:53 +0530

    updated frontend

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///train_seats.db'
+db = SQLAlchemy(app)
+
+# Define the Seat model
+class Seat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_age = db.Column(db.Integer, nullable=False)
+    preferred_age_group = db.Column(db.String(20), nullable=False)
+    seat_number = db.Column(db.Integer, nullable=False)
+    row = db.Column(db.Integer, nullable=False)
+    column = db.Column(db.Integer, nullable=False)
+    seat_type = db.Column(db.String(20), nullable=False)
 
 
 
@@ -10,18 +22,18 @@ def home():
     return render_template('index.html')
     #return 'Hello, World!'
 
-@app.route('/trains')
-def train_selection():
-    return render_template('trains.html')
+@app.route('/passenger')
+def passenger():
+    return render_template('passenger.html')
 
-@app.route('/login')
-def login_page():
-    return render_template('login.html')
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 
-@app.route('/book')
-def book_form():
-    return render_template('book.html')
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
 
 
 @app.route('/confirmation', methods=['POST'])
@@ -36,8 +48,15 @@ def confirmation():
     # Pass the form data to the confirmation page
     return render_template('confirmation.html', name=name, phone=phone, age=age, gender=gender, age_group=age_group)
 
-@app.route('/seating')
-def seating():
-    return render_template('seating.html')
+@app.route('/seat')
+def seat():
+    # available_seats = Seat.query.filter_by(is_booked=False).all()
+    # seats = Seat.query.all()  # Fetch all seats
+    return render_template('seat.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
